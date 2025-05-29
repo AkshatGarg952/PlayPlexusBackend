2fb6d7e6a2ebda81837af5aa1202554cc8125442
akshat45 <145849325+AkshatGarg952@users.noreply.github.com>
2025-05-29 21:28:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ import cors from 'cors'
 
 dotenv.config();
 const app = express();
+app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
 app.use('/api/users', userRouter);
 app.use('/api/teams', teamRouter);
 app.use('/api/requests', requestRouter);
