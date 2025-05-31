aee67da860cb903135c18b89779682e43d9d7133
akshat45 <145849325+AkshatGarg952@users.noreply.github.com>
2025-05-31 16:30:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/api/chatBot', chatBotRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', 
+    origin: 'https://play-plexus-frontend.vercel.app/', 
     methods: ['GET', 'POST'],
     credentials: true,
   },
